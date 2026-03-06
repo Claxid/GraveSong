3ae6983769633f21434dbf540e2383f13adbfe93
Claxid <hohohjiji651@gmail.com>
2026-03-06 15:42:30 +0100

    commentaire dans html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
+ 
+
 document.getElementById('myAudio').volume = 0.6;
 
 
 document.addEventListener('click', function() {
     document.getElementById('myAudio').muted = false;
   }, { once: true });
+
+
+  
