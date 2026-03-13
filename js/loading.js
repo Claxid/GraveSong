a90e2d1e42ad96fee3145a2764a9792935b09d29
Elsa13131 <elsachochon@gmail.com>
2026-03-13 16:26:16 +0100

    ajout css hmtl et script page loading

---
+++ js/loading.js
@@ -0,0 +1,8 @@
+// Afficher la page de chargement pendant 5 secondes
+window.addEventListener('load', function() {
+    // Attendre 5 secondes avant de rediriger
+    setTimeout(function() {
+        // Rediriger vers la page principale (index.html)
+        window.location.href = '../index.html';
+    }, 5000);
+});
