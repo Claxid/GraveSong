b308ea2df6a44ea2e7b0628565a1c879054568fb
Claxid <hohohjiji651@gmail.com>
2026-03-16 15:14:36 +0100

    Sprite changé

--- js/jeu/player.js
+++ js/jeu/player.js
@@ -1,6 +1,6 @@
 function createPlayerController(canvas, ctx, camera) {
     const sprite = new Image();
-    sprite.src = "../assets/sprites/Characters(100x100)/Soldier/Soldier/Soldier.png";
+    sprite.src = "../assets/sprites/Characters(100x100)/Soldier/Soldier/Soldier-Walk.png";
 
     const keys = {};
     document.addEventListener("keydown", (e) => {
@@ -24,11 +24,18 @@ function createPlayerController(canvas, ctx, camera) {
         frameX: 0,
         frameY: 0,
         frameSize: 100,
-        maxFrames: 6,
+        maxFrames: 8,
         animCounter: 0,
         animSpeed: 10,
         scale: 2
     };
+
+    let facingLeft = false;
+
+    sprite.addEventListener("load", () => {
+        // Adapt to the actual number of frames in the loaded walk sprite sheet.
+        player.maxFrames = Math.max(1, Math.floor(sprite.width / player.frameSize));
+    });
 
     function update() {
         let moving = false;
@@ -40,17 +47,19 @@ function createPlayerController(canvas, ctx, camera) {
         }
         if (keys["s"]) {
             player.y += player.speed;
-            player.frameY = 1;
+            player.frameY = 0;
             moving = true;
         }
         if (keys["q"]) {
             player.x -= player.speed;
-            player.frameY = 2;
+            player.frameY = 0;
+            facingLeft = true;
             moving = true;
         }
         if (keys["d"]) {
             player.x += player.speed;
-            player.frameY = 3;
+            player.frameY = 0;
+            facingLeft = false;
             moving = true;
         }
 
@@ -69,6 +78,27 @@ function createPlayerController(canvas, ctx, camera) {
         const size = player.frameSize * player.scale * camera.zoom;
         const drawX = (player.x - camera.x) * camera.zoom - size / 2;
         const drawY = (player.y - camera.y) * camera.zoom - size / 2;
+
+        if (facingLeft) {
+            ctx.save();
+            ctx.translate(drawX + size, drawY);
+            ctx.scale(-1, 1);
+
+            ctx.drawImage(
+                sprite,
+                player.frameX * player.frameSize,
+                player.frameY * player.frameSize,
+                player.frameSize,
+                player.frameSize,
+                0,
+                0,
+                size,
+                size
+            );
+
+            ctx.restore();
+            return;
+        }
 
         ctx.drawImage(
             sprite,
